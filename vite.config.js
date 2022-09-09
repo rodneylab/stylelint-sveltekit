30f1085be8b1ea59fd453bd7e0329b0ad483501f
Rodney Lab <codesign@rodneylab.com>
2022-09-09 09:28:06 +0100

    fix: 💫 update for SvelteKit breaking changes

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ const config = {
     'process.env.VITE_BUILD_TIME': JSON.stringify(new Date().toISOString()),
   },
   plugins: [sveltekit()],
+  server: {
+    port: 5173,
+    strictPort: false,
+  },
+  preview: {
+    port: 4173,
+    strictPort: false,
+  },
 };
 
 export default config;
